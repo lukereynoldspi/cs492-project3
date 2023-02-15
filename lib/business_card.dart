6ec97bdd13aae0b6bf381f4ba20221315a2c79b1
lukereynoldspi <lukereynoldspi@gmail.com>
2023-02-14 21:35:35 -0800

    finished business card

--- lib/business_card.dart
+++ lib/business_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:url_launcher/url_launcher.dart';
+
 class BusinessCard extends StatelessWidget {
   const BusinessCard({super.key});
 
@@ -12,23 +14,68 @@ class BusinessCard extends StatelessWidget {
           alignment: Alignment.topCenter,
           child: Padding(
             padding: const EdgeInsets.all(50),
-            child: SizedBox(
-              height: 50,
-              width: 50,
-              child: Placeholder(
-                child: Column(
-                  children: const [
-                    Text("Hello"),
-                    Text("Hello"),
-                    Text("Hello"),
-                    Text("Hello"),
-                  ],
-                ),
+            child: Column(children: [
+              const SizedBox(
+                height: 50,
+                width: 50,
+                child: Placeholder(),
               ),
-            ),
+              const Padding(
+                  padding: EdgeInsets.all(15), child: Text("Luke Reynolds")),
+              const Text("OSU Student, Creative Clown"),
+              const Text("\n123 456 7890\n"),
+              SizedBox(
+                width: 600,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      InkWell(
+                        onTap: _launchURL,
+                        child: Text(
+                          'My Github',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 243, 33, 33),
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: _launchEmail,
+                        child: Text(
+                          'My Email',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 243, 33, 33),
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+            ]),
           ),
         ),
       ),
     );
   }
+}
+
+_launchURL() async {
+  Uri url = Uri.parse('lukereynolds.github.io');
+  if (await launchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchEmail() {
+  final Uri emailLaunchUri = Uri(
+    scheme: 'mailto',
+    path: 'our.email@gmail.com',
+    queryParameters: {
+      'subject': 'Testing subject',
+      'body': 'Testing body text'
+    },
+  );
+  launchUrl(emailLaunchUri);
 }
